96990509aa4e9c23f457ee072eecbf4b7a4bdbb3
Anshuman Bhaduri <anshuman@bhadurian.com>
2020-12-23 10:13:46 +1300

    Update Summary story(ies) to use argTypes

--- src/components/Summary.stories.js
+++ src/components/Summary.stories.js
@@ -1,114 +1,93 @@
 import React from "react";
 import { Summary } from "./Summary";
 
+import "./Summary.stories.scss";
+
 export default {
     title: "Summary",
     component: Summary,
+    argTypes: {
+        numItems: {
+            name: "number of items",
+            defaultValue: 4,
+            control: {
+                type: "number",
+                min: 1,
+                max: 7,
+                step: 1,
+            },
+        },
+        containerWidth: {
+            name: "container width",
+            defaultValue: "100%",
+            control: {
+                type: "inline-radio",
+                options: ["200px", "350px", "600px", "790px", "1000px", "100%"],
+            },
+        },
+        labelLength: {
+            name: "label length",
+            defaultValue: "medium",
+            control: {
+                type: "inline-radio",
+                options: ["medium", "large", "extra large"],
+            },
+        },
+        valueLength: {
+            name: "value length",
+            defaultValue: "medium",
+            control: {
+                type: "inline-radio",
+                options: ["medium", "large", "extra large"],
+            },
+        },
+        outlines: {
+            name: "outlines",
+            defaultValue: false,
+            control: "boolean",
+        },
+    },
 };
 
-const Container = ({ width, children }) => {
+const createData = (numItems, labelLength, valueLength) => {
+    const items = Array(numItems).fill(0);
+    const lengths = {
+        medium: { label: "Average label", value: "Average value" },
+        large: { label: "Extra long key here", value: "Extra long value here" },
+        "extra large": {
+            label: "A Super long label here bro",
+            value: "A super duper long value here bro",
+        },
+    };
+    return items.map((item) => {
+        return {
+            label: lengths[labelLength].label,
+            value: lengths[valueLength].value,
+        };
+    });
+};
+
+const Template = ({
+    numItems,
+    containerWidth,
+    labelLength,
+    valueLength,
+    outlines,
+}) => {
+    const items = createData(numItems, labelLength, valueLength);
+    const className = `summary-template${
+        outlines ? " summary-template--with-outlines" : ""
+    }`;
     return (
         <div
+            className={className}
             style={{
-                width: width,
-                border: "1px solid black",
-                "box-sizing": "border-box",
+                width: containerWidth,
             }}
         >
-            {children}
+            <Summary items={items} />
         </div>
     );
 };
 
-const item = { label: "Average label", value: "Average value" };
-const lKey = { label: "Extra long key here", value: "Average value" };
-const lValue = { label: "Average label", value: "Extra long value here" };
-const xlKey = { label: "A Super long label here bro", value: "Average value" };
-const xlValue = {
-    label: "Average label",
-    value: "A super duper long value here bro",
-};
-const lBoth = { label: "Extra long key here", value: "Extra long value here" };
-const xlBoth = {
-    label: "A Super long label here bro",
-    value: "A super duper long value here bro",
-};
-
-const test = (items, width) => {
-    return () => (
-        <Container width={width}>
-            <Summary items={items} />
-        </Container>
-    );
-};
-
-export const XSContainerWithOneItem = test([item], "200px");
-export const XSContainerWithLessItems = test([item, lBoth], "200px");
-export const XSContainerWithSomeItems = test([item, lBoth, xlBoth], "200px");
-export const XSContainerWithFiveItems = test(
-    [item, lBoth, xlBoth, xlValue, lKey],
-    "200px"
-);
-
-export const SContainerWithLessItems = test([item, xlKey], "350px");
-export const SContainerWithSomeItems = test([item, lValue, xlBoth], "350px");
-export const SContainerWithFiveItems = test(
-    [item, lBoth, item, xlValue, lKey],
-    "350px"
-);
-export const SContainerWithSixItems = test(
-    [item, lBoth, xlBoth, xlValue, lKey, item],
-    "350px"
-);
-
-export const MContainerWithLessItems = test([item, xlKey], "600px");
-export const MContainerWithSomeItems = test([item, lValue, xlBoth], "600px");
-export const MContainerWithFiveItems = test(
-    [item, lBoth, item, xlValue, lKey],
-    "600px"
-);
-export const MContainerWithSixItems = test(
-    [item, lBoth, xlBoth, xlValue, lKey, item],
-    "600px"
-);
-
-export const LContainerWithLessItems = test([item, xlKey], "790px");
-export const LContainerWithSomeItems = test([item, lValue, xlBoth], "790px");
-export const LContainerWithFiveItems = test(
-    [item, lBoth, item, xlValue, lKey],
-    "790px"
-);
-export const LContainerWithSixItems = test(
-    [item, lBoth, xlBoth, xlValue, lKey, item],
-    "790px"
-);
-
-export const XLContainerWithLessItems = test([item, xlKey], "1000px");
-export const XLContainerWithSomeItems = test([item, lValue, xlBoth], "1000px");
-export const XLContainerWithFiveItems = test(
-    [item, lBoth, item, xlValue, lKey],
-    "1000px"
-);
-export const XLContainerWithSixItems = test(
-    [item, lBoth, xlBoth, xlValue, lKey, item],
-    "1000px"
-);
-export const XLContainerWithSevenItems = test(
-    [item, lBoth, xlBoth, item, xlValue, lKey, item],
-    "1000px"
-);
-
-export const FullContainerWithLessItems = test([item, xlKey], "100%");
-export const FullContainerWithSomeItems = test([item, lValue, xlBoth], "100%");
-export const FullContainerWithFiveItems = test(
-    [item, lBoth, item, xlValue, lKey],
-    "100%"
-);
-export const FullContainerWithSixItems = test(
-    [item, lBoth, xlBoth, xlValue, lKey, item],
-    "100%"
-);
-export const FullContainerWithSevenItems = test(
-    [item, lBoth, xlBoth, item, xlValue, lKey, item],
-    "100%"
-);
+export const Display = Template.bind({});
